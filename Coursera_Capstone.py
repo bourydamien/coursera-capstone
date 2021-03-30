22329edbf676e3d9ace3ff399f71d62a11a76714
bourydamien <81621656+bourydamien@users.noreply.github.com>
2021-03-30 23:45:48 +0200

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone project
+
+# #### This Notebook will be mainly used for my capstone project from the Data Science IBM Data Science Professionnal Certificate
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
